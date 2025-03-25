d2217c4de9ef65416a33c7ac42764f71dd1e1cd6
Joe <42866685+JoeRibeiro@users.noreply.github.com>
2025-03-25 12:25:18 +0000

    Update assess_maskrcnn.py

--- assess_maskrcnn.py
+++ assess_maskrcnn.py
@@ -78,7 +78,7 @@ def address_gauges_with_more_than_four_points(points, eps=50, min_samples=1):
 def get_model_instance_segmentation(num_classes):
     model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
     in_features = model.roi_heads.box_predictor.cls_score.in_features
-    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+    model.roid_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
     in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
     hidden_layer = 256
     model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask, hidden_layer, num_classes)
